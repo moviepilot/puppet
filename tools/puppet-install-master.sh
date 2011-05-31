472d6250fd410bdc9322624ae94379a610eeb116
Caspar Clemens Mierau <ccm@screenage.de>
2011-05-31 14:00:41 +0200

    added puppet bootstrap init scripts

---
+++ tools/puppet-install-master.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+# refresh package list
+apt-get update
+# bootstrap ruby env
+apt-get -q install irb libopenssl-ruby libreadline-ruby rdoc ri ruby ruby-dev git-core
+
+# get a working gem version and update it to the most recent one
+cd /usr/local/src
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.5.2.tgz
+tar -xzf rubygems-1.5.2.tgz
+cd rubygems-1.5.2
+ruby setup.rb
+update-alternatives --install /usr/bin/gem gem /usr/bin/gem1.8 1
+gem update --system
+
+# install puppet itself
+gem install puppet --no-ri --no-rdoc
+
+# we want sqlite3 and rails for stored configs on the puppet master
+apt-get -q install sqlite3 libsqlite3-ruby libsqlite3-dev
+gem install sqlite3-ruby --no-ri --no-rdoc
+gem install rails -v 2.3.11 --no-ri --no-rdoc
